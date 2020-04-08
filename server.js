2cf8f63e284fa831bf1ff1ccb5451deeab88b367
maisjamil1 <maisjamil17118@gmail.com>
2020-04-08 17:42:35 +0300

    33

--- server.js
+++ server.js
@@ -7,58 +7,60 @@
 'use strict';
 require('dotenv').config();
 const express = require('express');
-const pg = require('pg');
-// const cors = require('cors');_______
-
-const PORT = process.env.PORT || 4000;
 const app = express();
-// const superagent= require('superagent')_____
-// app.use(cors());_____
-
+const PORT = process.env.PORT || 4000;
+const cors = require('cors');
+ const superagent= require('superagent')
+const pg = require('pg');
 const client = new pg.Client(process.env.DATABASE_URL);
-client.on('error', (err) => {
-  throw new Error(err);
-});
-client
-  .connect()
-  .then(() => {
-    app.listen(PORT, () =>
-      console.log(`my server is running on port ${PORT}`)
-
-    );
-  })
-  .catch((err) => {
-    throw new Error(`startup error ${err}`);
-  });
-
-//مصيبة 2_____________________
-
-app.get('/location',(req,res)=>{
-
-  let search_querysql = locationData.search_query
-  let formatted_querysql = locationData.latitude
-  let latitudesql = locationData.latitude
-  let longitudesql = locationData.longitude
-  const SQL='INSERT INFO locations(search_query,value) VALUES ($1,$2) RETURNING *'
-  const safeValue=['search_query',search_querysql];
-  client
-  .query(SQL,safeValue)
-  .then((results) => {
-    res.status(200).json(results.rows);
-  })
+app.use(cors());
 
 
-  .catch((err) => {
-    res.status(500).send(err);
-  });
-
-
-   
-  });
-  
 
 
 //مصيبة 2_____________________
+
+// app.get('/location', (req, res) => {
+
+//   let search_querysql = locationData.search_query
+//   let formatted_querysql = locationData.latitude
+//   let latitudesql = locationData.latitude
+//   let longitudesql = locationData.longitude
+//   const SQL = 'INSERT INFO locations(search_query,value) VALUES ($1,$2) RETURNING *'
+//   const safeValue = ['search_query', search_querysql];
+//   client
+//     .query(SQL, safeValue)
+//     .then((results) => {
+//       res.status(200).json(results.rows);
+//     })
+
+
+//     .catch((err) => {
+//       res.status(500).send(err);
+//     });
+
+// });
+
+
+
+//مصيبة 2_____________________
+
+app.get('/location', (request, response) => {
+  const city = request.query.city;
+  const SQL = 'SELECT * FROM location WHERE search_query =$1';
+  const values =[city];
+client.query(SQL,values).then((result)=>{
+  response.status(200).json(result.rows);
+})
+.catch(()=>
+app.use((error,request,response)=>{
+  response.status(500).send(error);
+})
+);
+
+
+
+
 
 
 // app.get('/location', locationHandler);
@@ -86,12 +88,7 @@ app.get('/location',(req,res)=>{
 // }
 
 
-// function Location(city, geoData) {
-//   this.search_query = city;
-//   this.formatted_query = geoData[0].display_name;
-//   this.latitude = geoData[0].lat;
-//   this.longitude = geoData[0].lon;
-// }
+
 
 // //____________________
 
@@ -110,10 +107,7 @@ app.get('/location',(req,res)=>{
 //   }
 
 
-//   function Weather(day) {
-//     this.forecast = day.weather.description;
-//     this.time = new Date(day.valid_date).toString().slice(0, 15);
-//   }
+
 
 // //___________________________________________________
 
@@ -134,19 +128,7 @@ app.get('/location',(req,res)=>{
 //    };
 
 
-//    function Trail(trail$$) {
-//     this.name = trail$$.name;
-//     this.location = trail$$.location ;
-//     this.length = trail$$.length ;
-//     this.stars = trail$$.stars ;
-//     this.star$votes = trail$$.star_votes ;
-//     this.summary = trail$$.summary ;
-//     this.trail$url = trail$$.trail_url ;
-//     this.conditions= trail$$.conditions;
-//     this.conditions_date=trail$$.condition_date;
-//     this.conditions_time=trail$$.condtion_time
 
-// }
 
 
 
@@ -157,13 +139,105 @@ app.get('/location',(req,res)=>{
 //   response.status(404).send('page Not Found');
 // }
 
-// function errorHandler(error, request, response) {
-//   response.status(500).send(error);
-// }
+
+
+// constructors_______________________
+function Location(city, geoData) {
+  this.search_query = city;
+  this.formatted_query = geoData[0].display_name;
+  this.latitude = geoData[0].lat;
+  this.longitude = geoData[0].lon;
+}
+
+   function Weather(day) {
+     this.forecast = day.weather.description;
+     this.time = new Date(day.valid_date).toString().slice(0, 15);
+  }
+
+  function Trail(trail$$) {
+    this.name = trail$$.name;
+    this.location = trail$$.location ;
+    this.length = trail$$.length ;
+    this.stars = trail$$.stars ;
+    this.star$votes = trail$$.star_votes ;
+    this.summary = trail$$.summary ;
+    this.trail$url = trail$$.trail_url ;
+    this.conditions= trail$$.conditions;
+    this.conditions_date=trail$$.condition_date;
+    this.conditions_time=trail$$.condtion_time
+}
+
+ function errorHandler(error, request, response) {
+   response.status(500).send(error);
+ }
+
+// client.on('error', (err) => {
+//   throw new Error(err);
+// });
+
+client
+  .connect()
+  .then(() => {
+    app.listen(PORT, () =>
+      console.log(`my server is running on port ${PORT}`)
+
+    );
+  })
+  .catch((err) => {
+    throw new Error(err);
+  });
 
 
 
-// app.listen(PORT,() => console.log('host :' , PORT))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//________________________________
+
+client
+  .query(SQL, values)
+  .then((result) => {
+    if (result.rows.length > 0) {
+      response.status(200).json(result.rows[0]);
+    }else {
+        superagent(`https://eu1.locationiq.com/v1/search.php?key=${process.env.GEOCODE_API_KEY}&q=${city}&format=json`
+        ).then((result) => {
+          const geoData = res.body;
+          const locationData = new Location(city, geoData);
+          const SQL = 'INSERT INTO location (search_query,formatted_query,latitude,longitude)VALUES($1,$2,$3,$4) RETURNING *';
+const values =[
+  locationData.search_query,
+  locationData.formatted_query,
+  locationData.latitude,
+  locationData.longitude,
+];
+client.query(SQL,values).then((result)=>{
+  console.log(result.rows);
+  
+  response.status(200).json(result.rows[0]);
+});
+
+        });
+
+  //     }
+
+  //   }
+  // }
+  // )
 
 
 
